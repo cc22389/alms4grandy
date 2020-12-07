7979c55203513a81b70f7101428ccae4badbdffe
HeatherAckerman <hla20115213@gmail.com>
2020-12-07 09:53:33 -0600

    edited Modal wording

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -26,9 +26,9 @@ export default function App() {
                 closeTimeoutMS={500}
             >
                 <div style={{ textAlign: "center" }}>
-                    <p className="mobile" style={{ fontWeight: "bold", textDecoration: "underline" }}>DO NOT ENTER ANY PAYMENT INFORMATION!</p>
+                    <p className="mo" style={{ fontWeight: "bold", textDecoration: "underline" }}>DO NOT ENTER ANY PAYMENT INFORMATION!</p>
                     <p>This is a mock website created by students for a project.</p>
-                    <p>To learn more about these students, click <a href="https://github.com/cc22389/alms4grandy" target="blank" ><button style={{ background: "rgb(45,49,66)", color: "white", padding: "10px", border: "2px solid lightGray", borderRadius: "10px", fontSize: "85%" }}> here </button></a> and see the 'Contributors' section.</p><br />
+                    <p>To learn more about these students, click <a href="https://github.com/cc22389/alms4grandy" target="blank" ><button style={{ background: "rgb(45,49,66)", color: "white", padding: "10px", border: "2px solid lightGray", borderRadius: "10px", fontSize: "85%" }}> here </button></a> and see the contributors section.</p><br />
                     <div style={{ textAlign: "center" }}>
                         <img src={Us} alt="Contributors" style={{ border: "2px solid lightGray", borderRadius: "10px" }} /><br /><br /><br />
                     </div>
